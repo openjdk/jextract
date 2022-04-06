26055ff25e914ae217633ad8b10e5d8c41e071bc
Maurizio Cimadamore <maurizio.cimadamore@oracle.com>
2022-04-06 16:52:50 +0100

    Update copyright

--- src/main/java/org/openjdk/jextract/impl/JavaSourceBuilder.java
+++ src/main/java/org/openjdk/jextract/impl/JavaSourceBuilder.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2021, 2022, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -32,7 +32,6 @@ import org.openjdk.jextract.Declaration;
 import org.openjdk.jextract.Type;
 
 import javax.tools.JavaFileObject;
-import java.lang.invoke.MethodType;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
