d68a258c2226eb97cc698d7d3c0d7946a1470721
Sundararajan Athijegannathan <sundararajan.athijegannathan@oracle.com>
2022-04-12 19:14:39 +0530

    -C "-D FORCE_OPENBLAS_COMPLEX_STRUCT" does not work. For now changing it to -C-DFORCE_OPENBLAS_COMPLEX_STRUCT

--- samples/cblas/compile.sh
+++ samples/cblas/compile.sh
@@ -1,4 +1,4 @@
-jextract -C "-D FORCE_OPENBLAS_COMPLEX_STRUCT" \
+jextract -C-DFORCE_OPENBLAS_COMPLEX_STRUCT \
   -I /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/usr/include \
   -l openblas -t blas /usr/local/opt/openblas/include/cblas.h
 
