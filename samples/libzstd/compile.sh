1b1e3bc782c570d870754ce9a42797134d6fd762
Per Minborg <pminborg@gmail.com>
2022-08-30 15:30:13 +0200

    Simplify compile

--- samples/libzstd/compile.sh
+++ samples/libzstd/compile.sh
@@ -1,7 +1,6 @@
 jextract -t libzstd \
-  -I /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/usr/include \
   -I /usr/local/Cellar/zstd/1.5.2/include \
   -l zstd \
   --header-class-name Libzstd \
-  zstd.h
+    /usr/local/Cellar/zstd/1.5.2/include/zstd.h
 
--- samples/libzstd/compilesource.sh
+++ samples/libzstd/compilesource.sh
@@ -1,5 +1,4 @@
 jextract --source -t libzstd \
-  -I /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/usr/include \
   -I /usr/local/Cellar/zstd/1.5.2/include \
   -l zstd \
   --header-class-name Libzstd \
