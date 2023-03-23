c610bd36e400b821b8480915ebcd5b8bfd4a66d3
Sundararajan Athijegannathan <sundararajan.athijegannathan@oracle.com>
2023-03-23 19:03:50 +0530

    TCL sample fails because generated code is too large. But we only use 3 functions. Using filtering fixes the same jextraction step.

--- samples/tcl/compile.sh
+++ samples/tcl/compile.sh
@@ -1,3 +1,6 @@
 jextract -l tcl -t org.tcl \
+  --include-function Tcl_CreateInterp \
+  --include-function Tcl_Eval \
+  --include-function Tcl_DeleteInterp \
   -I /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/usr/include \
    /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/usr/include/tcl.h
