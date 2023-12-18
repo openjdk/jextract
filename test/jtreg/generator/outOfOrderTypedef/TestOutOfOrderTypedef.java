5e6545ea18878ad5480385abe38097e5fe9717a9
Maurizio Cimadamore <maurizio.cimadamore@oracle.com>
2023-12-18 16:15:25 +0000

    Fix out of order test

--- test/jtreg/generator/outOfOrderTypedef/TestOutOfOrderTypedef.java
+++ test/jtreg/generator/outOfOrderTypedef/TestOutOfOrderTypedef.java
@@ -1,4 +1,4 @@
-package generator.outOfOrderTypedef;/*
+/*
  * Copyright (c) 2023, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
@@ -50,6 +50,7 @@ public class TestOutOfOrderTypedef {
     }
 
     void checkLayout(GroupLayout layout) {
-        assertEquals(layout.memberLayouts().get(0), Foo.Baz.$LAYOUT());
+        assertEquals(layout.memberLayouts().get(0).withoutName(),
+                Foo.Baz.$LAYOUT().withoutName());
     }
 }
