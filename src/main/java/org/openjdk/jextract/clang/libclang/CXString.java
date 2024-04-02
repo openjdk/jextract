c24224e09ef81f581ec6948e5781b35b8453b682
James Yuzawa <jtyuzawa@gmail.com>
2024-04-02 16:01:24 +0000

    Fix javadoc generation and add check to testing suite
    Reviewed-by: jvernee

--- src/main/java/org/openjdk/jextract/clang/libclang/CXString.java
+++ src/main/java/org/openjdk/jextract/clang/libclang/CXString.java
@@ -181,7 +181,7 @@ public class CXString {
     }
 
     /**
-     * Reinterprets {@code addr} using target {@code arena} and {@code cleanupAction) (if any).
+     * Reinterprets {@code addr} using target {@code arena} and {@code cleanupAction} (if any).
      * The returned segment has size {@code layout().byteSize()}
      */
     public static MemorySegment reinterpret(MemorySegment addr, Arena arena, Consumer<MemorySegment> cleanup) {
@@ -189,7 +189,7 @@ public class CXString {
     }
 
     /**
-     * Reinterprets {@code addr} using target {@code arena} and {@code cleanupAction) (if any).
+     * Reinterprets {@code addr} using target {@code arena} and {@code cleanupAction} (if any).
      * The returned segment has size {@code elementCount * layout().byteSize()}
      */
     public static MemorySegment reinterpret(MemorySegment addr, long elementCount, Arena arena, Consumer<MemorySegment> cleanup) {
