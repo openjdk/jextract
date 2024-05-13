c74e689afbf857ee8eaf1c04cd9bb313c07ac066
Maurizio Cimadamore <maurizio.cimadamore@oracle.com>
2024-05-13 14:16:30 +0100

    Initial push

--- src/main/jextract.bat
+++ src/main/jextract.bat
@@ -1,2 +1,2 @@
-@set DIR=%~dp0
-@"%DIR%\runtime\bin\java" %JEXTRACT_JAVA_OPTIONS% -m org.openjdk.jextract/org.openjdk.jextract.JextractTool %*
+@set ROOT=%~dp0..
+@"%ROOT%\runtime\bin\java" %JEXTRACT_JAVA_OPTIONS% -m org.openjdk.jextract/org.openjdk.jextract.JextractTool %*
