a28666a5af4dfec85de39c3c369dac85bdb540c0
Ana <mihalceanuana@gmail.com>
2024-05-30 16:16:47 +0200

    Shorten the path to GLUT and OpenGL.

--- samples/opengl/compile.sh
+++ samples/opengl/compile.sh
@@ -1,3 +1,3 @@
-jextract -t opengl -l :/System/Library/Frameworks/GLUT.framework/Versions/Current/GLUT \
-  -l :/System/Library/Frameworks/OpenGL.framework/Versions/Current/OpenGL \
+jextract -t opengl -l :/System/Library/Frameworks/GLUT.framework/GLUT \
+  -l :/System/Library/Frameworks/OpenGL.framework/OpenGL \
   /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/System/Library/Frameworks/GLUT.framework/Headers/glut.h
