e1c67362825cf821b0608f7d3fab002dc68952f8
Ana <mihalceanuana@gmail.com>
2024-05-30 09:26:24 +0200

    Add source 22 and class output directory.

--- samples/opengl/compilesource.sh
+++ samples/opengl/compilesource.sh
@@ -2,4 +2,4 @@ jextract -t opengl -l :/System/Library/Frameworks/GLUT.framework/Versions/Curren
   -l :/System/Library/Frameworks/OpenGL.framework/Versions/Current/OpenGL \
   /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/System/Library/Frameworks/GLUT.framework/Headers/glut.h
 
-javac opengl/*.java
+javac --source=22 -d . opengl/*.java
