f9866df3a178baa2e4a3746a7a6e458d783ace92
Frederik van der Els <fred@xpple.dev>
2026-08-21 13:43:55 +0000

    7904209: Support variable array parameters
    Reviewed-by: jvernee

---
+++ test/testng/org/openjdk/jextract/test/toolprovider/VlaFunctionParameterTest.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright (c) 2026, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+
+package org.openjdk.jextract.test.toolprovider;
+
+import org.testng.SkipException;
+import org.testng.annotations.Test;
+import testlib.JextractToolRunner;
+import testlib.TestUtils;
+
+import java.io.IOException;
+import java.lang.foreign.FunctionDescriptor;
+import java.nio.file.Path;
+
+import static org.testng.Assert.*;
+
+public class VlaFunctionParameterTest extends JextractToolRunner {
+
+    @Test
+    public void testVlaFunctionParameter() throws IOException {
+        if (IS_WINDOWS) {
+            throw new SkipException("VLAs are not supported by MSVC");
+        }
+        Path output = getOutputFilePath("vlaFunctionParameter_out");
+        Path input = getInputFilePath("vlaFunctionParameter.h");
+        runAndCompile(output,
+            "-t", "org.jextract",
+            "-l", "VlaFunctionParameter", "--use-system-load-library",
+            input);
+        try (TestUtils.Loader loader = TestUtils.classLoader(output)) {
+            Class<?> cls = loader.loadClass("org.jextract.vlaFunctionParameter_h");
+            assertNotNull(cls);
+            FunctionDescriptor actualDescriptor = findDescriptor(cls, "foo");
+
+            FunctionDescriptor expectedDescriptor = FunctionDescriptor.of(
+                C_INT,
+                C_INT,
+                C_POINTER
+            );
+
+            assertEquals(actualDescriptor, expectedDescriptor);
+        } finally {
+            TestUtils.deleteDir(output);
+        }
+    }
+}
